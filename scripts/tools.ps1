c228e4d23dcdf946a52413db8c27d601e4153acb
MaryKroustali <mariakroust.mk.mk@gmail.com>
2024-12-29 17:24:32 +0200

    deploy app + db

--- scripts/tools.ps1
+++ scripts/tools.ps1
@@ -1,1 +1,4 @@
 # Install az cli
+Invoke-WebRequest -Uri https://aka.ms/installazurecliwindowsx64 -OutFile .\AzureCLI.msi; 
+Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; 
+az --version
